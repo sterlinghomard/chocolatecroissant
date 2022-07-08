9d8fdacf849f1237d5bed10c2ceec3e30be6fd5e
Dan <drblack651@gmail.com>
2022-07-08 07:16:16 -0500

    escape single quotes

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Home() {
       
         <main className='flex-grow'>
           <Picture />
-          <h1 className="text-2xl sm:text-3xl font-bold">Hi! I'm Dan&nbsp;Black 👋</h1>
+          <h1 className="text-2xl sm:text-3xl font-bold">Hi! I&apos;m Dan&nbsp;Black 👋</h1>
           <p>
-            I'm a full-stack software engineer at <a className="text-violet-800 hover:underline underline-offset-8 decoration-wavy font-semibold" href="https://www.chrobinson.com/en-us/" target="_blank" rel="noreferrer">CH&nbsp;Robinson</a> 
+            I&apos;m a full-stack software engineer at <a className="text-violet-800 hover:underline underline-offset-8 decoration-wavy font-semibold" href="https://www.chrobinson.com/en-us/" target="_blank" rel="noreferrer">CH&nbsp;Robinson</a> 
             working on the <a className="text-violet-800 hover:underline underline-offset-8 decoration-wavy font-semibold" href="https://www.freightquote.com/" target="_blank" rel="noreferrer">Freightquote</a> application.
           </p>
           <p>A couple quick things I enjoy:</p>
@@ -25,7 +25,7 @@ export default function Home() {
             <li>☀️ Sunshine</li>
             <li>💻 Learning new things</li>
           </ul>
-          <p>Currently, I'm living in Minneapolis but the cold and I do not get along particularly well so I've set my sights on warmer climates.</p>
+          <p>Currently, I&apos;m living in Minneapolis but the cold and I do not get along particularly well so I&apos;ve set my sights on warmer climates.</p>
           <p>Feel free to shoot me an email anytime!</p>
           <a href="mailto:drblack651@gmail.com" className="text-violet-800 hover:underline underline-offset-8 decoration-wavy font-semibold">📫&nbsp;drblack651@gmail.com</a>
         </main>
