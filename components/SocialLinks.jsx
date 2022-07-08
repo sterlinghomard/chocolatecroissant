aa9c5e918f2964795046c01272749b2422a7fec0
Dan <drblack651@gmail.com>
2022-07-07 21:14:09 -0500

    add rel='noreferrer' to make vercel happy

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -2,21 +2,21 @@ import Image from "next/image";
 
 export const SocialLinks = () => (
   <footer className="relative h-12">
-    <a href="https://www.linkedin.com/in/danielblack2022/" target='_blank'>
+    <a href="https://www.linkedin.com/in/danielblack2022/" target='_blank' rel="noreferrer">
       <Image 
         src='/icons/logo-linkedin.svg'
         alt='LinkedIn Icon'
         layout="fill"
       />
     </a>
-    <a href="https://github.com/daniel-black" target='_blank'>
+    <a href="https://github.com/daniel-black" target='_blank' rel="noreferrer">
       <Image 
         src='/icons/logo-github.svg'
         alt='Github Icon'
         layout="fill"
       />
     </a>
-    <a href="https://www.instagram.com/danblack__/" target='_blank'>
+    <a href="https://www.instagram.com/danblack__/" target='_blank' rel="noreferrer">
       <Image 
         src='/icons/logo-instagram.svg'
         alt='Instagram Icon'
