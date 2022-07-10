8032041c824443febd22826de7b7082526fd3aae
Dan <drblack651@gmail.com>
2022-07-10 12:43:47 -0500

    experimenting with different styling

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,10 @@ export default function Home() {
         <Picture />
         <h1 className="text-2xl sm:text-3xl font-bold font-jslab">Hi! I&apos;m Dan&nbsp;Black 👋</h1>
         <p>
-          I&apos;m a full-stack software engineer at <a className="text-violet-800 hover:underline underline-offset-8 decoration-wavy font-semibold" href="https://www.chrobinson.com/en-us/" target="_blank" rel="noreferrer">CH&nbsp;Robinson</a> working on the <a className="text-violet-800 hover:underline underline-offset-8 decoration-wavy font-semibold" href="https://www.freightquote.com/" target="_blank" rel="noreferrer">Freightquote</a> application.
+          I&apos;m a full-stack software engineer at <a className="link-style" href="https://www.chrobinson.com/en-us/" target="_blank" rel="noreferrer">
+            CH&nbsp;Robinson
+          </a> working on the <a className="link-style" href="https://www.freightquote.com/" target="_blank" rel="noreferrer">
+            Freightquote</a> application.
         </p>
         <p>A couple quick things I enjoy:</p>
         <ul>
@@ -27,7 +30,7 @@ export default function Home() {
         </ul>
         <p>Currently, I&apos;m living in Minneapolis but the cold and I do not get along particularly well so I&apos;ve set my sights on warmer climates.</p>
         <p>Feel free to shoot me an email anytime!</p>
-        <a href="mailto:drblack651@gmail.com" className="text-violet-800 hover:underline underline-offset-8 decoration-wavy font-semibold">📫&nbsp;drblack651@gmail.com</a>
+        <a href="mailto:drblack651@gmail.com" className="link-style">📫&nbsp;drblack651@gmail.com</a>
       </main>
 
     </div>
